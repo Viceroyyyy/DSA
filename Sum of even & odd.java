deb3aa395c62b8aeeacc501362262ac0b9717fdd
Viceroyyyy <120083048+Viceroyyyy@users.noreply.github.com>
2024-04-29 23:24:27 +0530

    Step-1 Learning Basics

---
+++ Sum of even & odd.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+	
+	public static void main(String[] args) {
+		// Write your code here
+		int even=0,odd=0,num,digit=0;
+		Scanner a= new Scanner(System.in);
+		num = a.nextInt();
+		while(num!=0)
+		{
+			digit=num%10;
+			num=num/10;
+			if(digit%2==0)
+			even+=digit;
+			else
+			odd+=digit;
+		}
+		System.out.print(""+even+" "+odd);
+
+	}
+}
