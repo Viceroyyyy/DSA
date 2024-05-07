b5adc6ddac7937849397b0124dcabbd9b2956c89
Viceroyyyy <120083048+Viceroyyyy@users.noreply.github.com>
2024-05-07 23:35:23 +0530

    Know Basic Maths

---
+++ Check Armstrong.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int arm = sc.nextInt();
+		int count=0;
+		int rem,sum=0,temp;
+		temp=arm;
+		int temp1=arm;
+		while(temp!=0)
+		{
+			count++;
+			temp=temp/10;
+
+		}
+		while(arm!=0)
+		{
+			rem=arm%10;
+			arm=arm/10;
+			sum=sum+(int)Math.pow(rem,count);
+		}
+		if(sum==temp1)
+		System.out.println("true");
+		else
+		System.out.println("false");
+		
+
+	}
+}
