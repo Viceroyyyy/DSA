7fbc30096c514c7d337ab7cab7239d2f4b6b52fe
Viceroyyyy <120083048+Viceroyyyy@users.noreply.github.com>
2024-10-01 23:56:00 +0530

    Array List Study

---
+++ Collections/Arrlis.java
@@ -0,0 +1,18 @@
+// package Collections;
+/**
+ * Arraylist
+ */
+// import java.util.ArrayList;
+
+import java.util.*;
+public class Arrlis {
+
+    public static void main(String[] args){
+       
+        ArrayList<String> list = new ArrayList<>();
+
+    }
+}
+    
+
+    
